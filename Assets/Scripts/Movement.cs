726c92265bbf091da944ce1fca90e97c75ae78e2
Mateusz <kuncewicz.mateusz@gmail.com>
2025-03-28 20:32:38 +0100

    Podstwa

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform player;
+    public float speed;
+    public float x;
+    public float z;
+
+    public AudioSource steps;
+
+    public Animator cameraAnimator;
+
+    void Start()
+    {
+         steps = gameObject.GetComponent<AudioSource>();
+    }
+
+   
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        player.Translate(x * speed * Time.deltaTime, 0, z * speed * Time.deltaTime);
+
+        if (x != 0 || z != 0)
+        {
+            steps.volume = 0.5f;
+            cameraAnimator.SetInteger("walk", 1);
+        }
+        else {
+            steps.volume = 0f;
+            cameraAnimator.SetInteger("walk", 0);
+        }
+    }   
+}
